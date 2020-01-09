2ff5241ab38891c9ad5e5fd97b47cddd4b2819e2
sultonexe <47837180+sultonexe@users.noreply.github.com>
2020-01-09 19:58:25 +0700

    Create detail.blade.php
    Template Detail (Crud) Laravel

---
+++ detail.blade.php
@@ -0,0 +1,68 @@
+@extends('template.app') //Me-Load template dari folder views/template/app.blade.php
+
+@section('title','Detail Product') //Section untuk title di file app.blade.php
+@section('pagetitle','Detail Product') //Section untuk pagetitle di file app.blade.php
+
+@section('content') //Section untuk content di file app.blade.php
+<div class="box box-primary">
+    <div class="box-body">
+
+        <div>
+            <a href="{{ route('product.index') }}" class="btn btn-sm btn-primary">
+                <i class="fa fa-arrow-left"></i> Kembali
+            </a>
+        </div> <br>
+
+        <div class="row">
+            <div class="col-md-6">
+                <dl class="dl-horizontal">
+                    <dt>Nama Produk</dt>
+                    <dd> {{ ucfirst($product->product) }}</dd>
+
+                    <dt>Harga Produk</dt>
+                    <dd> {{ "Rp. ". number_format($product->price,0,'.','.') }}</dd>
+
+                    <dt>Stok Produk</dt>
+                    <dd> <label for="" class="text text-primary"> {{ $product->stock }} </label></dd>
+
+                    {{-- <dt>Description</dt>
+                    <dd style="word-break: break-all;"> {{ str_limit($product->description,500,' ...') }}</dd> --}}
+
+                </dl>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <label for="">Description</label>
+                <p class="text">{!! $product->description !!}</p>
+            </div>
+        </div>
+
+        {{-- <div class="row"> --}}
+            <div class="table">
+                <table class="table table-striped table-hover table-responsive" id="table">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Image</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($product->imageRelation as $image)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>
+                                    <img src="{{ URL::asset('uploads/'.$image->image) }}" alt="gambar product" width="140px" height="160px">
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            {{-- </div> --}}
+
+    </div>
+</div>
+@endsection
+
